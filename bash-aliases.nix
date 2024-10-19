5dd6446b01ba03bee9f0a9877ddc1628fbac537b
Dimic Tech <support@dimic.tech>
2024-10-19 22:14:40 +0200

    Create bash-aliases.nix

---
+++ bash-aliases.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, lib, ...}:
+
+{
+    programs = {
+        command-not-found.enable = false;
+
+        bash = {
+            shellAliases = {
+                garbage = "sudo nix-collect-garbage --delete-older-than 3d";
+                edit = "sudo vim /etc/nixos/configuration.nix";
+                update = "sudo nixos-rebuild switch";
+                adminedit = "sudo code --no-sandbox --user-data-dir /etc/nixos/";
+            };
+        };
+
+    };
+}
